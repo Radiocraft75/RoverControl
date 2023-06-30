bd0479cc43ee039483ed71941b5f278e34560791
Radiocraft75 <bugzero@mail.ru>
2023-06-30 18:04:23 +0900

    feat: add OTA upload

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
 #include "BluetoothSerial.h"
+#include <WiFi.h>
+#include <ArduinoOTA.h>
 
 // ########################## DEFINES ##########################
 #define HOVER_SERIAL_BAUD   115200      // [-] Baud rate for HoverSerial (used to communicate with the hoverboard)
@@ -18,9 +20,14 @@
 #define DEBUG_RX                        // [-] Debug received data. Prints all bytes to serial (comment-out to disable)
 #define LED_BUILTIN 2
 
-//#define HoverSerial_2 Serial3;
-//SoftwareSerial HoverSerial_1(9, 10);       // RX, TX
-//SoftwareSerial HoverSerial_2(11, 12);
+#define SSID "KVANT_24"
+#define WifiPass "75367536"
+
+IPAddress local_IP(192, 168, 0, 40);
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress primaryDNS(8, 8, 8, 8); //optional
+IPAddress secondaryDNS(8, 8, 4, 4); //optional
 
 BluetoothSerial SerialBT;
 HardwareSerial SerialPort1(1);
@@ -245,6 +252,38 @@ void setup(){
   Serial.println("The device started, now you can pair it with bluetooth!");
   HoverSerial_1.begin(15200, SERIAL_8N1, 4, 2);
   HoverSerial_2.begin(15200, SERIAL_8N1, 16, 17); 
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+    Serial.println("STA Failed to configure");
+  }
+
+  Serial.print("Connecting to ");
+  Serial.println(SSID);
+
+  WiFi.begin(SSID, WifiPass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("ESP Mac Address: ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("Subnet Mask: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway IP: ");
+  Serial.println(WiFi.gatewayIP());
+  Serial.print("DNS: ");
+  Serial.println(WiFi.dnsIP());
+
+  // Port defaults to 3232
+  ArduinoOTA.setPort(3232);
+  // Hostname defaults to esp3232-[MAC]
+  ArduinoOTA.setHostname("Rover");
+  ArduinoOTA.begin();
 }
 
 unsigned long iTimeSend = 0;
@@ -252,6 +291,7 @@ int iTest = 0;
 int iStep = SPEED_STEP;
 
 void loop(void) {
+  ArduinoOTA.handle();
   // Если есть данные
   if (SerialBT.available()) {
     // Читаем команды и заносим их в переменную. char преобразует код символа команды в символ
